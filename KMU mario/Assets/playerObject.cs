740cdc7a48e4cffaf14dcd42ea75b1be520ec02d
eunju1215 <eunju1215@kookmin.ac.kr>
2017-11-15 17:07:11 +0900

    움직임 함수 만들기/레이어 조정

--- KMU mario/Assets/playerObject.cs
+++ KMU mario/Assets/playerObject.cs
@@ -3,100 +3,182 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerObject : MonoBehaviour {
-
-    public enum eSprite {
+public class PlayerObject : MonoBehaviour
+{
+    public enum eSprite
+    {
         Idle,
         Run,
         Jump
     }
 
     [SerializeField]
+    private Rigidbody2D _rigidbody = null;
+
+    [SerializeField]
     private SpriteRenderer _spriteIdle = null;
     [SerializeField]
     private SpriteRenderer _spriteRun = null;
-    [SerializeField] 
+    [SerializeField]
     private SpriteRenderer _spriteJump = null;
 
-    private StateMachine<playerObject> _stateMachine = null;
+    private StateMachine<PlayerObject> _stateMachine = null;
+
+    [SerializeField]
+    private float _speed = 5.0f;
+    [SerializeField]
+    private float _jump = 10.0f;
+
+    private bool isJump
+    {
+        get
+        {
+            return 0 != _rigidbody.velocity.y;
+        }
+    }
+
+    void Start()
+    {   
+
+        _stateMachine = new StateMachine<PlayerObject>(this);
+        StartCoroutine(_stateMachine.Coroutine<IdleState>());
+    }
 
 
+    private void Jump()
+    {
+        if (!isJump)
+        {
+            _rigidbody.AddForce(new Vector2(0, _jump));
+        }
+    }
 
-    void Start () {
-        _stateMachine = new StateMachine<playerObject>(this);
-        StartCoroutine(_stateMachine.Coroutine<Idlestate>());
-;	}
-    //시작하는 상태머신
 
-    private void HideAllsprite() {
+    private void Move(bool bLeft)
+    {
+        _rigidbody.AddForce(new Vector2((bLeft ? -_speed : _speed), 0));
+    }
+
+    private void LookAt(bool bLeft)
+    {
+        transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.x, bLeft ? 180 : 0, transform.rotation.z));
+    }
+
+    private void HideAllSprite()
+    {
         _spriteIdle.enabled = false;
         _spriteRun.enabled = false;
         _spriteJump.enabled = false;
     }
 
-    private void Showsprite(eSprite type) {
 
-        HideAllsprite();
+    private void ShowSprite(eSprite type)
+    {
+        HideAllSprite();
 
-        switch (type) {
+        switch (type)
+        {
             case eSprite.Idle:
                 _spriteIdle.enabled = true;
                 break;
+
             case eSprite.Run:
                 _spriteRun.enabled = true;
                 break;
+
             case eSprite.Jump:
                 _spriteJump.enabled = true;
                 break;
         }
     }
-    // 각각의 enum을 이용하여서 sprite 를 껐다 켰다 하는 부분
 
-    private class Idlestate : State<playerObject>
+    private class IdleState : State<PlayerObject>
     {
         protected override void Begin()
         {
-            Owner.Showsprite(eSprite.Idle);
-        }
-        protected override void Update() //상태가 지속될때
-        {
-        }
-        protected override void End()
-        {
+            Owner.ShowSprite(eSprite.Idle);
         }
 
-    }
-
-    private class Runstate : State<playerObject>
-    {
-        protected override void Begin()
-        {
-            Owner.Showsprite(eSprite.Run);
-        }
         protected override void Update()
         {
+            if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+            {
+                Invoke<RunState>();
+            }
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                Invoke<JumpState>();
+            }
         }
+
         protected override void End()
         {
         }
-
     }
 
-    private class Jumpstate : State<playerObject>
+    private class RunState : State<PlayerObject>
     {
         protected override void Begin()
         {
-            Owner.Showsprite(eSprite.Jump);
+            Owner.ShowSprite(eSprite.Run);
         }
+
         protected override void Update()
         {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                Owner.Move(true);
+                Owner.LookAt(true);
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                Owner.Move(false);
+                Owner.LookAt(false);
+            }
+            else
+            {
+                Invoke<IdleState>();
+            }
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                Invoke<JumpState>();
+            }
         }
+
         protected override void End()
         {
         }
-
     }
 
-    // Update is called once per frame
+    private class JumpState : State<PlayerObject>
+    {
+        protected override void Begin()
+        {
+            Owner.Jump();
+            Owner.ShowSprite(eSprite.Jump);
+        }
 
+        protected override void Update()
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                Owner.Move(true);
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                Owner.Move(false);
+            }
+
+            if (!Owner.isJump)
+            {
+                Invoke<IdleState>();
+            }
+        }
+
+        protected override void End()
+        {
+        }
+    }
 }
